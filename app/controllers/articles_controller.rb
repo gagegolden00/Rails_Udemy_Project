d8b06b74f467d12214b0ce648899ec4d985ed4f8
Gage <gage@rubyshore.com>
2023-06-05 15:35:55 -0500

    added links for nav, added users index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,18 +3,14 @@ class ArticlesController < ApplicationController
 
   def show
   end
-
   def index
     @articles = Article.all
   end
-
   def new
     @article = Article.new
   end
-
   def edit
   end
-
   def create
     @article = Article.new(whitelist_params)
     @article.user = User.first
@@ -25,27 +21,22 @@ class ArticlesController < ApplicationController
       render 'new'
     end
   end
-
   def update
     if @article.update(whitelist_params)
       redirect_to @article, notice: "Article was updated successfully."
-    
     else
       render 'edit'
     end
   end
-
   def destroy
     @article.destroy
     redirect_to articles_path
   end
-
+  
   private
-
   def set_article
     @article = Article.find(params[:id])
   end
-
   def whitelist_params
     params.require(:article).permit(:title, :description)
   end
